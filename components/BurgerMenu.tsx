948f39c90e26c7c4ec02f6f819d2bc24f7ad5fcf
AHmedEMadEG <amoemo141996@gmail.com>
2024-10-31 22:13:47 +0300

    fix: updated the burger menu style

--- components/BurgerMenu.tsx
+++ components/BurgerMenu.tsx
@@ -2,7 +2,52 @@ const BurgerMenu = () => {
   return (
     <div className="dropdown dropdown-end">
       <div tabIndex={0} role="button">
-        <img src="/burger-menu.svg" width={40} height={40} className="ms-6" />
+        <svg
+          width="60"
+          height="80"
+          viewBox="0 0 60 80"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <defs>
+            <linearGradient
+              id="ladderGradient"
+              x1="0%"
+              y1="0%"
+              x2="100%"
+              y2="0%"
+            >
+              <stop offset="0%" stop-color="#1a1a1a" />
+              <stop offset="20%" stop-color="#1a4c95" />
+              <stop offset="50%" stop-color="#8b2f6b" />
+              <stop offset="100%" stop-color="#ff5c5c" />
+            </linearGradient>
+          </defs>
+
+          <rect
+            x="30"
+            y="25"
+            width="20"
+            height="5"
+            rx="2"
+            fill="url(#ladderGradient)"
+          />
+          <rect
+            x="20"
+            y="35"
+            width="30"
+            height="5"
+            rx="2"
+            fill="url(#ladderGradient)"
+          />
+          <rect
+            x="10"
+            y="45"
+            width="40"
+            height="5"
+            rx="2"
+            fill="url(#ladderGradient)"
+          />
+        </svg>
       </div>
       <ul
         tabIndex={0}
