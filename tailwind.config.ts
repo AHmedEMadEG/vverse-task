94229ea4baa2a16c5fddf8337f9a539c2e21161d
AHmedEMadEG <amoemo141996@gmail.com>
2024-10-31 11:49:34 +0300

    chore: clean up project structure and optimize index.tsx - Removed unnecessary files and unused lines from index.tsx - Restructured overall project folders for improved organization - Updated file organization to align with new structure

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,10 @@
 import type { Config } from "tailwindcss";
+import daisyui from "daisyui";
 
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./layouts/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
@@ -14,6 +16,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [daisyui],
 };
 export default config;
